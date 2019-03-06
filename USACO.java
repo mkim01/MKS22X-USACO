6445df60a9dcc8d94a11ba69147c0a93e58739e9
Minuk Kim <mkim01@stuy.edu>
2019-03-06 18:31:25 -0500

    made 2D array of lake

--- USACO.java
+++ USACO.java
@@ -2,21 +2,38 @@ import java.util.*;
 import java.io.*;
 
 public class USACO{
-  private int[][] board;
 
-  public static int bronze(String filename) throws FileNotFoundException{
+  public static String bronze(String filename) throws FileNotFoundException{
     File f = new File(filename);
     Scanner inf = new Scanner(f);
+    //Parseint the output
     int inputR = Integer.parseInt(inf.next());
     int inputC = Integer.parseInt(inf.next());
     int inputE = Integer.parseInt(inf.next());
     int inputN = Integer.parseInt(inf.next());
-    return inputN;
+    // 2D array
+    int[][] lake = new int[inputR][inputC];
+    for (int i = 0; i < inputR; i++){
+      for (int j = 0; j < inputC; j++){
+        lake[i][j] = Integer.parseInt(inf.next());
+      }
     }
+    //
+    String output = "";
+    for (int i = 0; i < inputR; i++){
+      for (int j = 0; j < inputC; j++){
+        output += lake[i][j] + " ";
+      }
+      output += "\n";
+    }
+    return output;
+  }
 
     public static void main (String[] args){
       try{
+        USACO u1 = new USACO();
         System.out.println(bronze("makelake.1.in"));
+      //  System.out.println(USACO.board);
       }
       catch(FileNotFoundException e){
         System.out.println("yongtong");
